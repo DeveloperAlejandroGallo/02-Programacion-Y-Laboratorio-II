ff146349c33c070e81f2025f39e0b9ea2f71a96c
Alejandro Gallo <alegallo@gmail.com>
2017-08-23 13:59:09 -0300

    Add files via upload

---
+++ Ejercicios/Ej16/Alumno.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej16
+{
+    public class Alumno
+    {
+        private static int Nota1;
+
+        private static int Nota2;
+
+        private static int NotaFinal;
+
+        string apellido;
+
+        int legajo;
+
+        string nombre;
+
+        public static void Estudiar(int nota1, int nota2)
+        {
+            Nota1 = nota1;
+            Nota2 = nota2;
+        }
+
+        public static void CalcularFinal()
+        {
+            Random random = new Random();
+
+            if (Nota1 >= 4 && Nota2 >= 4)
+                NotaFinal = random.Next(4, 10);
+            else
+                NotaFinal = -1;
+        }
+
+        public static void Mostrar()
+        {
+            Console.WriteLine("\nALUMNO\tNOTA 1\tNOTA 2\tNOTA FINAL");
+            Console.Write("{0}\t{1}\t{2}\t",1,Nota1,Nota2);
+            if(NotaFinal != -1)
+                Console.Write("{0}\n", NotaFinal);
+
+
+        }
+
+        public static Alumno setDatos(string nombre, string apellido, int legajo)
+        {
+            Alumno alumno=new Alumno();
+
+            alumno.apellido = apellido;
+            alumno.nombre = nombre;
+            alumno.legajo = legajo;
+
+            return alumno;
+        }
+
+
+    }
+}
---
+++ Ejercicios/Ej16/Ejercicio16.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej16
+{
+    class Ejercicio16
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno() ;
+            Alumno alumno2 = new Alumno();
+            Alumno alumno3 = new Alumno();
+            string apellido, nombre;
+            int legajo, nota1, nota2;
+
+            for(var i=1; i<=3;i++ )
+            {
+                Console.WriteLine("Ingrese los datos del {0}° alumno:");
+                Console.Write("Apellido: ");
+                apellido = Console.ReadLine();
+                Console.Write("Nombre: ");
+                nombre = Console.ReadLine();
+                Console.Write("Legajo: ");
+                while(!int.TryParse(Console.ReadLine(),out legajo))
+                    Console.Write("El legajo debe ser numerico!.\nLegajo: ");
+                Console.Write("Nota 1: ");
+                while (!int.TryParse(Console.ReadLine(), out nota1))
+                    Console.Write("La nota debe ser numerica!.\nNota 1: ");
+                Console.Write("Nota 2: ");
+                while (!int.TryParse(Console.ReadLine(), out nota2))
+                    Console.Write("La nota debe ser numerica!.\nNota 2: ");                
+
+                switch(i)
+                {
+                    case 1:
+                        alumno1 = Alumno.setDatos(nombre, apellido, legajo);
+                        break;
+                    case 2:
+                        alumno2 = Alumno.setDatos(nombre, apellido, legajo);
+                        break;
+                    case 3:
+                        alumno3 = Alumno.setDatos(nombre, apellido, legajo);
+                        break;
+                }
+                Alumno.Estudiar(nota1, nota2);
+
+                Alumno.Mostrar();
+                Console.WriteLine("\n");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
