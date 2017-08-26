6e7e9fa1dd669345ff47b371d5b97b46cbec6e08
Alejandro Gallo <alegallo@gmail.com>
2017-08-26 19:48:16 -0300

    Ej 17 y 18

--- Ejercicios/Ej18/Computadora.cs
+++ Ejercicios/Ej18/Computadora.cs
@@ -3,11 +3,82 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ej18_Enum;
+
 
 namespace Ej18_Class
 {
     public class Computadora
     {
-        bool estaEncendida
+        bool estaEncendida;
+
+        EMarca marca;
+
+        float peso;
+
+        EProcesador procesador;
+
+        public Computadora(bool encender, EMarca marca, float peso, EProcesador procesador)
+        {
+            this.estaEncendida = encender;
+            this.marca =  marca;
+            this.peso = peso;
+            this.procesador = procesador;
+
+        }
+
+        //Getters
+        public bool getEstaEncendida()
+        {
+            return this.estaEncendida;
+        }
+        public EMarca getMarca()
+        {
+            return this.marca;
+        }
+        public float getPeso()
+        {
+            return this.peso;
+        }
+        public EProcesador getProcesador()
+        {
+            return this.procesador;
+        }
+
+        //Setters
+        public void setEstaEncendida(bool encender)
+        {
+            this.estaEncendida = encender;
+        }
+        public void setMarca(EMarca marca)
+        {
+            this.marca = marca;
+        }
+        public void setPeso(float peso)
+        {
+            this.peso = peso;
+        }
+        public void setProcesador(EProcesador procesador)
+        {
+            this.procesador = procesador;
+        }
+
+        public void InformarEstado()
+        {
+            
+            Console.WriteLine("{0,-10}|{1,-10}|{2,-10}|{3,-10}", this.estaEncendida, this.marca, this.peso, this.procesador);
+
+        }
+
+        public void Encender()
+        {
+            this.estaEncendida = true;
+        }
+        public void Apagar()
+        {
+            this.estaEncendida = false;
+        }
+
+
     }
 }
--- Ejercicios/Ej18/Ejercicio18.cs
+++ Ejercicios/Ej18/Ejercicio18.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ej18_Class;
+using Ej18_Enum;
 
 namespace Ej18
 {
@@ -10,6 +12,15 @@ namespace Ej18
     {
         static void Main(string[] args)
         {
+            Computadora compu1 = new Computadora(true, EMarca.Apple, 1.2F, EProcesador.ADM_Semprom_145);
+
+            Console.WriteLine("{0,-10}|{1,-10}|{2,-10}|{3,-10}", "Encendida", "Marca", "Peso", "Procesador");
+            compu1.InformarEstado();
+            compu1.setPeso(3);
+            compu1.Apagar();
+            compu1.InformarEstado();
+            
+            Console.ReadKey();
         }
     }
 }
