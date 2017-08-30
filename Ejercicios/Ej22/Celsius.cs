cdade8afd9a1f2c9ab8b2b9c45a4bbdb6fb7ab02
Alejandro Gallo <alegallo@gmail.com>
2017-08-30 12:53:54 -0300

    Ej 22
    22

---
+++ Ejercicios/Ej22/Celsius.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperaturas
+{
+    class Celsius
+    {
+        double temperatura;
+
+        public Celsius()
+        {
+            temperatura = 1;
+        }
+    }
+}
---
+++ Ejercicios/Ej22/Ejercicio22.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej22
+{
+    class Ejercicio22
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
