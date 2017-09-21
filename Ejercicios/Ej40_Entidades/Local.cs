d78ff424b1301d80d8e40ed8a37639b11d6c2ce7
DeveloperAlejandroGallo <alegallo@gmail.com>
2017-09-21 00:53:20 -0300

    ej 40

---
+++ Ejercicios/Ej40_Entidades/Local.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej40_Entidades
+{
+    public class Local : Llamada
+    {
+        protected float costo;
+
+        #region Prop
+        public override float CostoLlamada
+        {
+            get { return CalcularCosto(); }
+        }
+        #endregion
+
+        #region Constructores
+        public Local(Llamada llamada, float costo)
+            : this(llamada.NroOrigen, llamada.Duracion, llamada.NroDestino, costo)
+        {
+            
+        }
+
+        public Local(string origen, float duracion, string destino, float costo) 
+            : base(duracion,destino,origen)
+        {
+            this.costo = costo;
+        }
+        #endregion
+
+        #region Metodos
+        protected override string Mostrar()
+        {
+            StringBuilder str = new StringBuilder();
+            str.AppendFormat("{0}|{1,-10}|{2,-10}\n", base.Mostrar(), "LOCAL", this.CostoLlamada);
+
+            return str.ToString();
+        }
+
+        private float CalcularCosto()
+        {
+            return (base.Duracion * this.costo);
+        }
+        
+
+        public static bool Equals(object obj)
+        {
+            return obj is Local;
+        }
+
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+
+        #endregion
+
+
+
+
+    }
+}
