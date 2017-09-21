126b9ab610ec9b34fa1d7c72d135bed4d3905dc3
DeveloperAlejandroGallo <alegallo@gmail.com>
2017-09-21 01:26:33 -0300

    Modif Dirs

---
+++ Ejercicios/Ej39/SobreSobreescrito.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej39
+{
+    public class SobreSobreescrito : Sobreescrito
+    {
+
+        public override string MiAtributo
+        {
+            get
+            {
+                return this.miAtributo;
+            }
+        }
+
+        public override string MiMetodo()
+        {
+            return "Mi Metodo en SobreSobreescrito";
+        }
+    }
+}
---
+++ Ejercicios/Ej39/Sobreescrito.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej39
+{
+    public abstract class Sobreescrito
+    {
+
+        protected string miAtributo;
+
+        #region Constructores
+        public Sobreescrito()
+        {
+            this.miAtributo = "Probar Abstractos";
+        } 
+        #endregion
+
+        #region Propiedades
+        public abstract string MiAtributo;
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return "¡Este es mi método ToString sobreescrito!";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (obj is Sobreescrito);
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+
+        public abstract string MiMetodo();
+
+        #endregion
+    }
+}
