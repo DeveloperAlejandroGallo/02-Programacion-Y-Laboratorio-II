3e1df97add9c1e2d31acc6d440812bf4dac40bee
DeveloperAlejandroGallo <alegallo@gmail.com>
2017-10-01 23:39:19 -0300

    Modificaciones

---
+++ Practica/PracticaConsole/PracticaConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short cantidad = 4;
+            Queue<int> cola = new Queue<int>();
+            Stack<int> pila = new Stack<int>();
+
+
+            for (int i = 0; i <= cantidad * 2; i += 2)
+                cola.Enqueue(i);
+
+            foreach (int i in cola)
+                pila.Push(i);
+
+            foreach (int i in pila)
+                Console.Write("{0}, ", i);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Practica/PracticaParcial/PracticaParcial1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace PracticaParcial1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "a",
+            "s",
+            "d",
+            "g",
+            "q",
+            "f",
+            "v",
+            "w",
+            "fsf",
+            "sf",
+            "xc",
+            "s",
+            "sd",
+            "sd",
+            "s",
+            "s",
+            "s",
+            "s",
+            "s",
+            "s",
+            "s",
+            "s"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "das",
+            "asd",
+            "f",
+            "vf",
+            "f",
+            "aef",
+            "qw",
+            "\t",
+            "q\t",
+            "we",
+            "qqwd",
+            "qw",
+            "wd",
+            "\twf",
+            "qwf"});
+            this.listBox1.Location = new System.Drawing.Point(90, 73);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
