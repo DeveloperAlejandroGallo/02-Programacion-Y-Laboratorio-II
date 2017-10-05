f12191c33d0f34ba244cf6e4cc97df61e85c81c8
Alejandro Gallo <alegallo@gmail.com>
2017-10-04 22:09:55 -0300

    laa

---
+++ Ejercicios/Ej42/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.InnerException);
+            }
+
+        }
+    }
+}
