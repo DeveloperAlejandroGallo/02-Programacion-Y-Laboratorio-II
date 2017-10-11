f64ec00371678dd4d2750af0e252d07554b9a073
DeveloperAlejandroGallo <alegallo@gmail.com>
2017-10-11 00:30:05 -0300

    ff

---
+++ Ejercicios/Ej40_Entidades/CentralitaException.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej40_Entidades
+{
+    public class CentralitaException : Exception
+    {
+        string nombreClase;
+        string nombreMetodo;
+
+        public string NombreClase
+        {
+            get
+            {
+                return nombreClase;
+            }
+        }
+
+        public string NombreMetodo
+        {
+            get
+            {
+                return nombreMetodo;
+            }
+        }
+
+
+        public Exception ExcepcionInterna
+        {
+            get 
+            {
+                return new Exception();
+            }
+        }
+
+        public CentralitaException(string mensaje, string clase, string metodo)
+        {
+            this.nombreMetodo = metodo;
+            this.nombreClase = clase;
+           
+        }
+    }
+}
