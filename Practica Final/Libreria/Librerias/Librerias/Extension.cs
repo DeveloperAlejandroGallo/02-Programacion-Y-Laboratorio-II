adccf20f7c8056337bd57399bdf99788abf3d0a7
DeveloperAlejandroGallo <alegallo@gmail.com>
2017-12-13 20:01:13 -0300

    MyLib

---
+++ Practica Final/Libreria/Librerias/Librerias/Extension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librerias
+{
+    public sealed class ClaseSellada
+    {
+        private int id;
+        public ClaseSellada(int id)
+        {
+            this.id = id;
+        }
+
+        public int ID
+        {
+            get
+            {
+                return this.id;
+            }
+        }
+    }
+    //**************************************************************************//
+    public static class ExtensionClaseSellada
+    {
+        public static string MetodoExtendido(this ClaseSellada miClase, string prefijo)
+        {
+            return String.Format("{1} <---> {0}", miClase.ID, prefijo);
+        }
+        public static string MetodoExtendido(this ClaseSellada miClase, string prefijo, string otro)
+        {
+            return String.Format("{1} <---> {0}", miClase.ID, prefijo);
+        }
+
+        public static string MetodoExtendido(this String miClase, string prefijo)
+        {
+            return String.Format("{1} <---> {0}", prefijo, miClase);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClaseSellada cs = new ClaseSellada(19);
+            String s = "Prefijo";
+
+            Console.WriteLine(cs.MetodoExtendido("Sufijo ClaseSellada1", "a"));
+            Console.WriteLine(cs.MetodoExtendido("Sufijo ClaseSellada2"));
+
+            Console.WriteLine(s.MetodoExtendido("Sufijo String"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practica Final/Libreria/Librerias/Librerias/Indexadores.cs
@@ -0,0 +1,100 @@
+﻿/*
+* Created by SharpDevelop.
+* User: i.landajuela
+* Date: 21/12/2016
+* Time: 18:34
+*
+* To change this template use Tools | Options | Coding | Edit Standard Headers.
+*/
+using System;
+
+namespace Indexadores
+{
+
+    //Ejemplo de uso de descriptores de acceso get/set para controlar
+    //el acceso a miembros privados de la clase
+    class TimePeriod
+    {
+        private double _seconds;
+        public double Seconds
+        {
+            get { return _seconds; }
+            set { _seconds = value; }
+        }
+    }
+
+    //-----------------------------------------------------------------
+
+    public class clsCaja
+    {
+        private string[] nombres = { "longitud", "altura", "anchura" };
+        private double[] medidas = new double[3];
+
+        public clsCaja(double longitud, double anchura, double altura)
+        {
+            medidas[0] = longitud;
+            medidas[1] = anchura;
+            medidas[2] = altura;
+        }
+
+        //indexador para acceder a medidas por el número de indice entero
+        public double this[int indice]
+        {
+            get
+            {
+                //valida indice a obtener
+                if ((indice < 0) || (indice >= medidas.Length))
+                {
+                    return -1;
+                }
+                else
+                    return medidas[indice];
+            } //fin get
+            set
+            {
+                if (indice >= 0 && indice < medidas.Length)
+                    medidas[indice] = value;
+            } //fin set
+
+        } // fin de indexador numérico
+
+        public double this[string nombre]
+        {
+            get
+            {
+                //localiza elemento a obtener
+                int i = 0;
+                while ((i < nombres.Length) && (nombre != nombres[i]))
+                    i++;
+                return (i == nombres.Length) ? -1 : medidas[i];
+            }
+            set
+            {
+                int i = 0;
+                while ((i < nombres.Length) && (nombre != nombres[i]))
+                    i++;
+                if (i != nombres.Length)
+                    medidas[i] = value;
+            }
+        } //fin de indexador string
+
+    }
+
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            clsCaja objCaja = new clsCaja(10, 20, 30);
+
+
+            objCaja[0] = 80;
+            Console.WriteLine("caja[\"longitud\"]={0}", objCaja["longitud"]);
+            objCaja["longitud"] = 90;
+            Console.WriteLine("caja[\"longitud\"]={0}", objCaja["longitud"]);
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
